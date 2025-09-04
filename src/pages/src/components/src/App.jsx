54a6e69b74b9457b76b8219c033ef1c832bbc7c8
dave-8bit <chinwenmerid@gmail.com>
2025-09-05 00:13:18 +0100

    Create App.jsx

---
+++ src/pages/src/components/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Shop from './pages/Shop'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
